7d6b582dd9581990449fe16bc4ce376cb6b6210a
uma mahesh Kadiyala <umamahesh833@gmail.com>
2021-04-04 16:20:19 +0100

    "updated purches success test case"

--- src/test/java/Tests/PurchaseSuccess.java
+++ src/test/java/Tests/PurchaseSuccess.java
@@ -4,6 +4,12 @@ public class PurchaseSuccess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("updated another tests");
+		System.out.println("updated another tests");
+		System.out.println("updated another tests");
+		System.out.println("updated another tests");
+		System.out.println("updated another tests");
 
 	}
 
